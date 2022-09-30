99bb9c464292179ca98066ff076ace842c16db42
Samuel Lubelsky <slubelsky30@stuy.edu>
2022-09-30 08:58:01 -0400

    copied code from partner

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -1,20 +1,28 @@
-'''
-Samuel Lubelsky
-Softdev
-K05 - Read, parse, and display contents of txt file containing pds, dev names, and ducky names
-2022-09-29
-time spent: 
-DISCO:
+"""
+Jeff Chen, Samuel Lubelsky
+SoftDev
+K05 - Read, parse, and display contents of txt files containing pds, dev names, and ducky names.
+2022-9-29
+time spent:
+DISCO: 
+Use "with open() as f" with filename, mode to open a file for parsing.
+inputstring = file.read() to turn input into a string
+file.read() returns contents of file
+strip removes specified string from front and back of string
 QCC:
+Why does my code still have a /n at the end even though I used strip???
+What are the modes for open()?
+"""
 
-'''
 import random
-with open('krewes.txt','r') as f:
+with open('krewes.txt', 'r') as f:
     inputString = f.read()
-devos = inputString.strip('\n').split("@@@")
+devos = inputString.strip('/n').split("@@@")
 for i in range(len(devos)):
-    devos[i] = devos[i].split("$$$")
+    devos[i] = devos[i].strip('/n').split("$$$")
 randomDevo = random.choice(devos)
 devoPeriod, devoName, duckName = randomDevo[0], randomDevo[1], randomDevo[2]
 print(f'{devoName} is in period {devoPeriod} and their ducky\'s name is {duckName}')
+
+
 
