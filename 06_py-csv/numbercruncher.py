5157456512eef4f5a2acaebaf086edbd7addcc75
Samuel Lubelsky <slubelsky30@stuy.edu>
2022-10-03 10:03:43 -0400

    in working state, going to put some tests in

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -6,7 +6,11 @@ K06 - Randomly select a job with weighted percentages
 time spent: 45 min
 DISCO: 
 Built in CSV reader
+del dict[key] deletes a key from dictionary
+csvreader is not a list, it is a special python object called an iterable
+that must be iterated through with a for loop
 QCC:
+what exactly is an iterable and can they be converted to lists?
 """
 
 jobs = {}
@@ -27,13 +31,23 @@ with open("./occupations.csv", 'r') as file:
 
   ##for e in jobs:
   ##    print(e)
+def choose_job(jobs: dict):
+    randomJob = random.random()*99.8
+    #print(randomJob)
+    chance = jobs[list(jobs)[0]]
+    for e in jobs:
+        if randomJob < chance:
+            chosenJob = e
+            break
+        chance = chance + jobs[e]
+    return chosenJob
+def printJob(job):
+    print("Chosen Job is " + job)
+for _ in range(100):
+    chosenJob = choose_job(jobs)
+    printJob(chosenJob)
+#additional tests
+num_tests = 1_000_000
+#for _ in range(num_tests):
 
-randomJob = random.random()*99.8
-print(randomJob)
 
-chance = jobs[list(jobs)[0]]
-for e in jobs:
-    if randomJob < chance:
-        print(e)
-        break
-    chance = chance + jobs[e]
